4dddbeb48eeb3fa12fa0f7b7d77e38d5b745b424
David Henley <henleydr@gmail.com>
2017-10-27 10:15:47 -0500

    Server code for Socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
 
 const app = express();
+const server = http.Server(app);
+const io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('Server listening on', PORT);
+});
+
+io.on('connection', (socket) => {
+  console.log('Hello');
 });
